7193da93951e5acef6c745f08ee1a97dc168a66f
ken koontz <kenneth.koontz@gmail.com>
2015-12-28 14:51:19 -0800

    add create session

--- build/app/app.module.js
+++ build/app/app.module.js
@@ -1,7 +1,17 @@
 angular
   .module('app', [])
   .controller('SessionsCtrl', function($scope) {
-    $scope.createSessionLink = function() {
-      $scope.link = 'http://session.ly/1234';
+    $scope.session = {
+      _id: '1234',
+      name: 'Session',
+      link: 'http://session.ly/1234'
+    };
+
+    $scope.createSession = function() {
+      $scope.session = $scope.session;
+    };
+
+    $scope.shareSessionLink = function() {
+      $scope.link = $scope.session.link;
     };
   });
